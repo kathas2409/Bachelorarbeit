10733a97b4e72b0e4d0b98a75159c4b3a5dbbc36
kathas2409 <94140197+kathas2409@users.noreply.github.com>
2025-06-12 20:37:44 +0200

    Update saveMessage.ts

--- functions/api/saveMessage.ts
+++ functions/api/saveMessage.ts
@@ -1,1 +1,9 @@
+export const onRequestPost: PagesFunction = async (context) => {
+  const body = await context.request.json();
+  console.log("Nachricht gespeichert:", body);
+  return new Response(JSON.stringify({ status: "OK" }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
 
